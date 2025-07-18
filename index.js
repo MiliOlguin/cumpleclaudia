db91bff0b65957d5c2cb324f5a7d0520c6789cef
claudia sammarco <miluaye.olguin@gmail.com>
2025-07-18 04:25:06 -0300

    mostrar formulario

---
+++ index.js
@@ -0,0 +1,6 @@
+const btnAceptar=document.getElementById("btn-aceptar")
+const form=document.getElementById("formulario")
+
+btnAceptar.addEventListener('click', function () {
+    form.style.display = 'block';
+  });
